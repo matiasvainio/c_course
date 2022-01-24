970cd6e84a16217bf4fe44cd7b70c43d578cca06
Matias Vainio <matias.s.vainio@gmail.com>
2022-01-24 19:07:08 +0200

    tehtävä 4 fixed

--- teht_4/teht_4.c
+++ teht_4/teht_4.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 void main() {
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr[10] = {0};
     int *p1, *p2;
     p1 = arr;
     p2 = &arr[(sizeof(arr) - 1) / sizeof(arr[0])];
 
-    // for (int i = 0; i < sizeof(arr) / sizeof(arr[0]) / 2; i++) {
-    //     printf("%d \n", *p);
-    //     *p += 1;
-    // }
-
+    printf("%s\n", "Syötä lukuja:");
     while (p1 - 1 != p2) {
         scanf("%d", p1);
         p1 += 2;
@@ -21,6 +17,7 @@ void main() {
 
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
         printf("%d ", *p1);
+        p1++;
     }
     printf("\n");
 
