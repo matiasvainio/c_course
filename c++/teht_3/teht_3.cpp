cf16d5dc336bc6ca6de99086fd06b6b3cf0258a2
Matias Vainio <matias.s.vainio@gmail.com>
2022-02-25 13:23:13 +0200

    cpp teht 3 done

--- c++/teht_3/teht_3.cpp
+++ c++/teht_3/teht_3.cpp
@@ -16,21 +16,59 @@ int main() {
 
 Matkakortti alustus() {
     Matkakortti mk;
+    string nimi;
+    double arvo;
+    cout << "Nimi: ";
+    cin >> nimi;
+    cout << "Arvo: ";
+    cin >> arvo;
+
+    mk.setNimi(nimi);
+    mk.setArvo(arvo);
+
     return mk;
 }
 
 int matkustus(Matkakortti* mk, double hinta) {
+    if (mk->getArvo() - hinta <= mk->getArvo()) {
+        mk->setArvo(mk->getArvo() - hinta);
+        cout << "Arvoa jäljellä " << mk->getArvo() << endl;
+    } else {
+        cout << "Arvo ei riitä." << endl;
+    }
     return 0;
 }
 
 void lataus(Matkakortti* mk) {
+    double arvo;
+    cout << "Arvon lataus:" << endl;
+    cout << "Anna arvo: ";
+    cin >> arvo;
+
+    mk->setArvo(mk->getArvo() + arvo);
+
+    cout << "Arvoa jäljellä: " << mk->getArvo() << endl;
 }
 
 void tulostaTiedot(Matkakortti* mk) {
+    cout << "Kortin tiedot." << endl;
+    cout << "Nimi: " << mk->getNimi() << endl;
+    cout << "Arvo: " << mk->getArvo() << endl;
+}
+
+int tarkistaKortinOlemeassaOlo(Matkakortti* ptr) {
+    if (ptr != NULL) {
+        return 1;
+    } else {
+        cout << "Matkakorttia ei ole luotu." << endl;
+        return 0;
+    }
 }
 
 void tulostaValikko() {
     char v;
+    Matkakortti mk;
+    Matkakortti* ptr = NULL;
 
     do {
         cout << "Valikko:" << endl;
@@ -46,17 +84,29 @@ void tulostaValikko() {
 
         switch (v) {
             case '1':
-                alustus();
+                mk = alustus();
+                ptr = &mk;
                 break;
             case '2':
-                // lataus();
+                if (tarkistaKortinOlemeassaOlo(ptr) == 1) {
+                    lataus(ptr);
+                }
                 break;
-                // case '3':
-                // // matkustus();
-                // case '4':
-                // // matkustus();
-                // case '5':
-                //     // tulostaTiedot();
+            case '3':
+                if (tarkistaKortinOlemeassaOlo(ptr) == 1) {
+                    matkustus(ptr, SISAINEN);
+                }
+                break;
+            case '4':
+                if (tarkistaKortinOlemeassaOlo(ptr) == 1) {
+                    matkustus(ptr, SEUTU);
+                }
+                break;
+            case '5':
+                if (tarkistaKortinOlemeassaOlo(ptr) == 1) {
+                    tulostaTiedot(ptr);
+                }
+                break;
             default:
                 break;
         }
